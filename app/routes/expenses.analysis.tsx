aecb8e32d1499709294842ddc38278fdef9b056f
Henrique Hartwig <henrique.hartwig@checkstore.com.br>
2025-01-07 20:56:38 -0300

    feat: add fake data

--- app/routes/expenses.analysis.tsx
+++ app/routes/expenses.analysis.tsx
@@ -2,6 +2,20 @@ import type { MetaFunction } from "@remix-run/node";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
 import Chart from "~/components/expenses/Chart";
 
+const FAKE_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date('2020-07-01'),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date('2021-02-12'),
+  },
+];
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,7 +26,7 @@ export const meta: MetaFunction = () => {
 
 export default function ExpensesAnalysis() {
   return <main>
-    <Chart expenses={[]} />
-    <ExpenseStatistics expenses={[]} />
+    <Chart expenses={FAKE_EXPENSES} />
+    <ExpenseStatistics expenses={FAKE_EXPENSES} />
   </main>;
 }
